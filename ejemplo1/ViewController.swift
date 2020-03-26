9d1effa0a4ecccdc58e55e038610e18cae4cc68b
Jeferson Cieza <jefferson.cieza@pucp.edu.pe>
2020-03-26 17:37:04 -0500

    Initial Commit

---
+++ ejemplo1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ejemplo1
+//
+//  Created by Jeferson Cieza on 3/26/20.
+//  Copyright © 2020 Jeferson Cieza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
